6a07e742d3b69bcc3a7539e4d77bfdd09dfe93a5
Fabio Cavadas Fernandes Ferreira <fabio.cavadas@g.globo>
2025-01-11 19:39:00 -0300

    add o componente menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,13 +1,17 @@
 <template>
-  <header class="menu">
-    <div class="logo">
-      <h1>Gestor de Mídias</h1>
-    </div>
-    <div class="auth-actions">
-      <button v-if="!isAuthenticated" @click="goToRegister">Registrar-se</button>
-      <button v-if="!isAuthenticated" @click="goToLogin">Login</button>
-      <button v-if="isAuthenticated" @click="logout">Sair</button>
-    </div>
+  <header class="menu-container">
+    <div class="menu-header">
+      <router-link to="/">
+          <img src="../assets/logo.png" alt="Logo Gestor de Mídias" />          
+        </router-link>        
+      <div >
+        
+      </div>
+      <h1>Gestor de Mídias Digitais</h1>
+      <div class="auth-actions">
+        <button @click="goToRegister">Registrar-se</button>        
+      </div>      
+    </div>    
   </header>
 </template>
 
@@ -16,36 +20,46 @@ export default {
   computed: {
     isAuthenticated() {
       return this.$store.state.auth.isAuthenticated;
-    }
+    },
   },
   methods: {
     goToRegister() {
-      this.$router.push('/register');
-    },
-    goToLogin() {
-      this.$router.push('/login');
-    },
-    logout() {
-      this.$store.dispatch('auth/logout');
-      this.$router.push('/');
+      this.$router.push("/register");
     }
-  }
+  },
 };
 </script>
 
 <style scoped>
-.menu {
+.menu-container {
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-  background-color: #333;
-  color: white;
+  align-items: left;
+  padding: 10px;  
+  color: rgb(0, 89, 255);
 }
+
+.header {
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  box-shadow: 0 2px 0 0 #333; 
+  margin-bottom: 20px; 
+}
+
+.logo-title {
+  display: flex;
+  align-items: center;
+}
+
+.auth-actions {
+  display: flex;
+  align-items: center; 
+}
+
 .auth-actions button {
-  margin-left: 10px;
+  margin-left: 10px; 
 }
 </style>
-
-
-
